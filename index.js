c9c14678d019262dee58f668ee4086f5c7dd760e
Henrynunez112 <henrynunez@pursuit.org>
2019-12-17 21:30:40 -0500

    done

--- index.js
+++ index.js
@@ -5,25 +5,44 @@ document.addEventListener("DOMContentLoaded",()=>{
     let form = document.querySelector("form");
     let content = document.querySelector(".content");
     let select = document.querySelector("select");
-    let limit = 10;
-    let search = "coffee"
     
     let h1 = document.createElement("h1");
-    form.addEventListener("submit",()=>{
+    
+    select.addEventListener("change",(el)=>{
+        select.value = el.target.value;
+        // debugger
+    })             
 
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        giphySearch(input.value, select.value);
+        console.log(input.value, select.value)
     })
+    
+    const showGif = (arr)=>{
+        arr.forEach(gif =>{
+            let img = document.createElement("img");
+            img.src = gif.images.downsized.url;
+            content.appendChild(img)
+            debugger;
 
+        })
+    }
+        
     const giphySearch = async (userInput, userLimit) =>{
         try{
-            let res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${search}&limit=${userLimit}`);
-            console.log(res)
+            content.innerHTML = ""
+            let res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${userInput}&limit=${userLimit}`);
+            let data = res.data.data
             debugger
-        }catch(error){
+            showGif(data);
+
+                debugger
+            }catch(error){
             console.log(error)
         }
     }
 
-    giphySearch(search,limit)
     const populateSelect = (search, limit) =>{
         // let i = 0;
         // while(i < 25){
@@ -32,7 +51,7 @@ document.addEventListener("DOMContentLoaded",()=>{
         //     select.appendChild(option);
         //     i++
         // }
-        for(let i = 1; i <= 25; i++){
+        for(let i = 0; i <= 25; i++){
             let option = document.createElement("option");
             option.innerText = i
             option.value = i
